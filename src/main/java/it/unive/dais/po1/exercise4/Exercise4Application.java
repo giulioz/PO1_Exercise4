de86901ee8655bc053de2f15278372144bf6cf6e
Giulio Zausa <mail.zausa.giulio@gmail.com>
2020-11-26 23:33:36 +0100

    Initial commit

---
+++ src/main/java/it/unive/dais/po1/exercise4/Exercise4Application.java
@@ -0,0 +1,13 @@
+package it.unive.dais.po1.exercise4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Exercise4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Exercise4Application.class, args);
+	}
+
+}
---
+++ src/test/java/it/unive/dais/po1/exercise4/Exercise4ApplicationTests.java
@@ -0,0 +1,13 @@
+package it.unive.dais.po1.exercise4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
